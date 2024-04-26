fb24b652eef91ca41112761ef1b96911f0684c9d
Tinodaishe Tembo <synthmancer@gmail.com>
2024-04-26 23:22:22 +0200

    soho

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/Link";
+
+const Footer = () => {
+	return (
+		<div className="flex items-center gap-10 py-16">
+			<Link></Link>
+		</div>
+	);
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { useState, useRef } from "react";
+import html2canvas from "html2canvas";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-csharp";
+
 const page = () => {
-	return <div>Hello World</div>;
+	return <div>I am building a ray.so clone</div>;
 };
 
 export default page;
